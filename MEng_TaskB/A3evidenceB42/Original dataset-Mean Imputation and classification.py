9909482c8eb8ed5076d56036c57828efbc319c07
Isa-dw <isabel.dw42@gmail.com>
2024-05-19 17:16:34 +0200

    Uploaded compressed files. Worked on submission files

---
+++ MEng_TaskB/A3evidenceB42/Original dataset-Mean Imputation and classification.py
@@ -0,0 +1,128 @@
+import pandas as pd
+
+
+# Load dataset
+file_path = r"C:\Users\difedile.rasenyalo\OneDrive - DSV\Stellenbosch\Post block Assignment3\train_data_control.xlsx"
+
+data = pd.read_excel(file_path)
+
+print(data)
+
+# Load dataset
+file_path = r"C:\Users\difedile.rasenyalo\OneDrive - DSV\Stellenbosch\Post block Assignment3\test_data.xlsx"
+
+data_test = pd.read_excel(file_path)
+
+print(data_test)
+
+
+# Model the orginal data set before imputing 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+
+
+# Split the data into features (X) and target (y)
+X_train = data.drop(columns=['target'])
+y_train = data['target']
+
+# Split the test data into features (X_test) and target (y_test)
+X_test = data_test.drop(columns=['target'])
+y_test = data_test['target']
+
+# Initialize Decision Tree classifier
+clf = DecisionTreeClassifier(random_state=42) #The value 42 is an arbitrary choice; you can use any integer value or leave it unspecified if you don't need reproducibility.
+
+# Train the classifier on the training data
+clf.fit(X_train, y_train)
+
+# Predict on the test data
+y_pred = clf.predict(X_test)
+
+# Calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+# Print classification report
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+
+# Print confusion matrix
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+#y_test and y_pred contain the true labels and predicted labels respectively
+# Define class labels
+classes = sorted(set(y_test) | set(y_pred))
+
+# Calculate confusion matrix
+cm = confusion_matrix(y_test, y_pred, labels=classes)
+
+
+# Plot confusion matrix
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, cmap='Oranges', fmt='d', xticklabels=classes, yticklabels=classes, cbar=False, annot_kws={"color": 'black'})
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+plt.title('Confusion Matrix')
+plt.show()
+
+### K- NN Classification 
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+
+# Define a range of k values
+k_values = range(1, 20)
+
+# Initialize empty list to store cross-validation scores
+cv_scores = []
+
+# Perform cross-validation for each value of k
+for k in k_values:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    scores = cross_val_score(knn, X_train, y_train, cv=5, scoring='accuracy')  # 5-fold cross-validation
+    cv_scores.append(scores.mean())
+
+# Find the optimal value of k with the highest cross-validation score
+optimal_k = k_values[cv_scores.index(max(cv_scores))]
+
+print("Optimal k:", optimal_k)
+
+###  K- NN Classification 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+# X_train, X_test, y_train, y_test are the training and testing data
+
+# Instantiate k-NN classifier
+knn = KNeighborsClassifier(n_neighbors=18) 
+
+# Train the classifier
+knn.fit(X_train, y_train)
+knn.fit(X_test, y_test)
+
+y_pred = knn.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+
+
+# Plot confusion matrix
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, cmap='Greens', fmt='d', xticklabels=classes, yticklabels=classes, cbar=False, annot_kws={"color": 'black'})
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+plt.title('Confusion Matrix - k-NN Classifier')
+plt.show()
+
+### Evaluate the model 
+
+accuracy_knn = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy_knn)
+print(classification_report(y_test, y_pred))
+
+
+
+
